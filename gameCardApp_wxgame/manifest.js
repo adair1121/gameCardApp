13f2780b58062b6148509363574a86f5cc2229f1
zhangzhiqiang <youxi123>
2020-09-08 10:12:19 +0800

    update

--- gameCardApp_wxgame/manifest.js
+++
@@ -1,7 +0,0 @@
-require("./js/egret.min.js")
-require("./js/eui.min.js")
-require("./js/assetsmanager.min.js")
-require("./js/tween.min.js")
-require("./js/game.min.js")
-require("./js/default.thm.js")
-require("./js/main.min.js")
